b3089b2ddd4905ea930601f67827680080fde482
Andreea Buzenchi <buzenchiandreea26@gmail.com>
2025-03-25 09:13:03 +0200

    KEY-26 Added a main page with sign in and sign up buttons

--- frontend/fly-with-me/src/App.jsx
+++ frontend/fly-with-me/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@mantine/core/styles.css'
+import { Button } from "@mantine/core";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="text">
+        <h1>FLY WITH <br /> ME</h1>
+        <p>Your next adventure starts here. Fast bookings, great prices, and smooth flights to the places you love</p>
+        <div className="buttons">
+        <Button >Sign In</Button>
+        <Button >Sign Up</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
